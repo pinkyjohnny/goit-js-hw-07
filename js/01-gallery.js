d714a4e64101057cecb996495cb074fa7c890449
pinkyjohnny <136741239+pinkyjohnny@users.noreply.github.com>
2023-08-13 21:10:33 +0300

    update-task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,17 +2,17 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const refs = {
-    galleryList: document.querySelector(".gallery"),
-    galleryLink: document.querySelector('.gallery__link')
+  galleryList: document.querySelector(".gallery"),
+  galleryLink: document.querySelector('.gallery__link')
 }
 
 
 function galleryTemplate(galleryItems) {
-    const preview = galleryItems.preview;
-    const original = galleryItems.original;
-    const description = galleryItems.description;
+  const preview = galleryItems.preview;
+  const original = galleryItems.original;
+  const description = galleryItems.description;
 
-    return `<li class="gallery__item">
+  return `<li class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
@@ -25,19 +25,20 @@ function galleryTemplate(galleryItems) {
 }
 
 function renderGallery(images) {
-    const markup = images.map(galleryTemplate).join('')
-    refs.galleryList.innerHTML = markup;
+  const markup = images.map(galleryTemplate).join('')
+  refs.galleryList.insertAdjacentHTML("afterbegin", markup)
+
 }
 
 renderGallery(galleryItems)
 
 
 document.querySelectorAll('.gallery__item').forEach((item, idx) =>
-    item.onclick = (event) => {
-        event.preventDefault();
-        basicLightbox.create(`
+  item.onclick = (event) => {
+    event.preventDefault();
+    basicLightbox.create(`
 		<img width="1400" height="900" src="${galleryItems[idx].original}">
 	`).show()
-    })
+  })
 
 console.log(galleryItems);
