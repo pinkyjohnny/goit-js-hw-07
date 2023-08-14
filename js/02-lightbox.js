17f7cce2e48821a1f6bea4a5e9cb0e2bdf8c18fc
pinkyjohnny <136741239+pinkyjohnny@users.noreply.github.com>
2023-08-14 15:33:22 +0300

    task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const refs = {
+    galleryList: document.querySelector(".gallery"),
+    galleryLink: document.querySelector('.gallery__link')
+}
+
+
+function galleryTemplate(galleryItems) {
+    const preview = galleryItems.preview;
+    const original = galleryItems.original;
+    const description = galleryItems.description;
+
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+}
+
+function renderGallery(images) {
+    const markup = images.map(galleryTemplate).join('')
+    refs.galleryList.insertAdjacentHTML("afterbegin", markup)
+
+}
+
+renderGallery(galleryItems)
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250
+});
+
+gallery.on('show.simplelightbox', function (event) {
+    event.preventDefault();
+});
+
 console.log(galleryItems);
